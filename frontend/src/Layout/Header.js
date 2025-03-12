521551e9bdba4b28624d7eae9ff8e3f379ed3ba0
UDAI BHANU <110327079+bhanuUdai@users.noreply.github.com>
2025-03-13 00:23:41 +0530

    fixed header and expense details

--- frontend/src/Layout/Header.js
+++ frontend/src/Layout/Header.js
@@ -11,15 +11,19 @@ import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { NavLink, useHistory, useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { authAction } from "../store/auth-reducer";
 import ToggleOffIcon from "@mui/icons-material/ToggleOff";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import Switch from "@mui/material/Switch";
 import { themeAction } from "../store/theme-reducer";
-import { useSelector } from "react-redux";
+import Button from "@mui/material/Button"; // Import Button
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 import appLogo from "../assets/images/logo.png";
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import { useTheme } from "@mui/material/styles";
 
 const pages = ["Home", "Expenses"];
 const settings = ["Profile", "Logout"];
@@ -27,13 +31,15 @@ const settings = ["Profile", "Logout"];
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const theme = useTheme();
+
 
   const history = useHistory();
   const dispatch = useDispatch();
   const location = useLocation();
   const projectId = location?.pathname?.split("/").pop();
   const isLogin = useSelector((state) => state.auth.token);
-
+  const expenseArr = useSelector((state) => state.expense.expenses); // Access expense state
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -59,13 +65,33 @@ function ResponsiveAppBar() {
       case "Profile":
         history.push(`/contact/${projectId}`);
         break;
-    
+
       default:
         break;
     }
   };
 
   const darkTheme = useSelector((state) => state.theme.theme);
+
+  const exportToPDF = () => {
+    const doc = new jsPDF();
+    doc.text("Expense Breakdown", 20, 20);
+
+    const tableColumn = ["Amount", "Description", "Category"];
+    const tableRows = [];
+
+    expenseArr.forEach((expense) => {
+      const expenseData = [
+        expense.amount,
+        expense.description,
+        expense.category,
+      ];
+      tableRows.push(expenseData);
+    });
+
+    autoTable(doc, { head: [tableColumn], body: tableRows, startY: 30 });
+    doc.save("expenses.pdf");
+  };
 
   return (
     <AppBar position="fixed">
@@ -85,43 +111,90 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            {/* <IconButton>
-              <ToggleOffIcon />
-            </IconButton> */}
-            <img src={appLogo} alt="logo" style={{ width: "50px", marginRight : "20px" }} />
+            <img
+              src={appLogo}
+              alt="logo"
+              style={{ width: "50px", marginRight: "20px" }}
+            />
           </Typography>
-         {isLogin && <>
+          {isLogin && (
+            <>
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={() => setAnchorElNav(null)}
+                  sx={{ display: { xs: "block", md: "none" } }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={() => setAnchorElNav(null)}>
+                      <NavLink
+                        to={
+                          page === "Home"
+                            ? `/welcome/${projectId}`
+                            : `/${page.toLowerCase()}/${projectId}`
+                        }
+                        activeClassName="active-link"
+                        style={{
+                          textDecoration: "none",
+                          color: "inherit",
+                        }}
+                      >
+                        <Typography textAlign="center">{page}</Typography>
+                      </NavLink>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            {/* <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton> */}
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={() => setAnchorElNav(null)}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page}>
+              <Typography
+                variant="h5"
+                noWrap
+                component="div"
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                {/* Mobile Logo or Title */}
+              </Typography>
+
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: { xs: "none", md: "flex" },
+                  gap: "10px",
+                }}
+              >
+                {pages.map((page) => (
                   <NavLink
+                    key={page}
                     to={
                       page === "Home"
                         ? `/welcome/${projectId}`
@@ -130,111 +203,88 @@ function ResponsiveAppBar() {
                     activeClassName="active-link"
                     style={{
                       textDecoration: "none",
-                      color: "inherit",
+                      color: "white",
+                      padding: "10px",
+                      borderRadius: "5px",
+                    }}
+                    activeStyle={{
+                      backgroundColor: "rgba(255, 255, 255, 0.2)",
                     }}
                   >
-                    <Typography textAlign="center">{page}</Typography>
+                    {page}
                   </NavLink>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+                ))}
+               {expenseArr.length > 0 && <Box sx={{
+                  marginLeft:"10px"
+                }} >
+                  <Tooltip title="Download expenses">
+                    <IconButton>
+                      <IconButton 
+                      onClick={exportToPDF}
+                      sx={{
+                        padding :"0px"
+                      }} >
+                         <FileDownloadOutlinedIcon  sx={{
+                            color:  "white" ,
+                          }} />
+                      </IconButton>
+                    </IconButton>
+                  </Tooltip>
+                </Box>}
 
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            
-          </Typography>
+                <Box>
+                <Tooltip title= {theme.palette.mode === "dark" ? "Light" : "Dark"}>
+                    <IconButton>
+                    {darkTheme ? <LightModeIcon  onClick={() => {
+                      dispatch(themeAction.toggleTheme());
+                    }} /> : <DarkModeIcon onClick={() => {
+                      dispatch(themeAction.toggleTheme());
+                    }} />}
+                    </IconButton>
+                  </Tooltip>
+                
+                </Box>
+              </Box>
 
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex" },
-              gap: "10px",
-            }}
-          >
-            {pages.map((page) => (
-              <NavLink
-                key={page}
-                to={
-                  page === "Home"
-                    ? `/welcome/${projectId}`
-                    : `/${page.toLowerCase()}/${projectId}`
-                }
-                activeClassName="active-link"
-                style={{
-                  textDecoration: "none",
-                  color: "white",
-                  padding: "10px",
-                  borderRadius: "5px",
-                }}
-                activeStyle={{
-                  backgroundColor: "rgba(255, 255, 255, 0.2)",
-                }}
-              >
-                {page}
-              </NavLink>
-            ))}
+             
 
-            <Box>
-              <IconButton>
-                {darkTheme ? <DarkModeIcon /> : <LightModeIcon />}
-              </IconButton>
-              <Switch
-                onClick={() => {
-                  dispatch(themeAction.toggleTheme());
-                }}
-                value={darkTheme}
-                color="default"
-              />
-            </Box>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Udai Bhanu" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={() => setAnchorElUser(null)}
-            >
-              {settings.map((setting) => (
-                <MenuItem
-                  key={setting}
-                  onClick={() => handleCloseUserMenu(setting)}
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Udai Bhanu"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={() => setAnchorElUser(null)}
                 >
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          </>}
+                  {settings.map((setting) => (
+                    <MenuItem
+                      key={setting}
+                      onClick={() => handleCloseUserMenu(setting)}
+                    >
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
--- frontend/src/pages/Expenses/ExpenseDetails.js
+++ frontend/src/pages/Expenses/ExpenseDetails.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import Expenses from './Expenses'
-import { Typography } from "@mui/material";
+import React from 'react';
+import Expenses from './Expenses';
+import { Typography } from "@mui/material"; // Removed Button import
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
+const ExpenseDetails = ({ expenseArr, editButtonHandler, deleteButtonHandler }) => {
 
-const ExpenseDetails = ({expenseArr, editButtonHandler, deleteButtonHandler}) => {
+  const exportToPDF = () => {
+    const doc = new jsPDF();
+    doc.text("Expense Breakdown", 20, 20);
+
+    const tableColumn = ["Amount", "Description", "Category"];
+    const tableRows = [];
+
+    expenseArr.forEach(expense => {
+      const expenseData = [
+        expense.amount,
+        expense.description,
+        expense.category,
+      ];
+      tableRows.push(expenseData);
+    });
+
+    autoTable(doc, { head: [tableColumn], body: tableRows, startY: 30 });
+    doc.save("expenses.pdf");
+  };
+
   return (
     <section>
-    <Typography variant='h5' fontWeight="bold" mb="20px" textAlign="center" > Breakdown </Typography>
-    {expenseArr.length > 0 && (
-      <Expenses
-        expenseArr={expenseArr}
-        key={Math.random()}
-        editButtonHandler={editButtonHandler}
-        deleteButtonHandler={deleteButtonHandler}
-      />
-    )}
-  </section>
-  )
+      <Typography variant='h5' fontWeight="bold" mb="20px" textAlign="center" color='#1976d2'>Breakdown</Typography>
+      {expenseArr.length > 0 && (
+        <>
+          {/* Removed Export to PDF Button */}
+          <Expenses
+            expenseArr={expenseArr}
+            key={Math.random()}
+            editButtonHandler={editButtonHandler}
+            deleteButtonHandler={deleteButtonHandler}
+          />
+        </>
+      )}
+    </section>
+  );
 }
 
-export default ExpenseDetails
+export default ExpenseDetails;
